6609f981e986f5fa4870f891feee6f4ce72dc16c
Brian Cavalier <brian@hovercraftstudios.com>
2015-08-21 07:45:07 -0400

    Improve then and map test coverage

---
+++ test/then-test.js
@@ -0,0 +1,31 @@
+import { delay, reject } from '../src/main';
+import { assertSame } from './lib/test-util';
+import assert from 'assert';
+
+describe('map', function() {
+
+    it('should not change value when f is not a function', () => {
+        let expected = {};
+        return delay(1, expected).then()
+            .then(x => assert.strictEqual(x, expected));
+    });
+
+    it('should not change reason when r is not a function', () => {
+        let expected = {};
+        return delay(1, expected).then(reject).then(x => null)
+            .then(assert.ifError, x => assert.strictEqual(x, expected));
+    });
+
+    it('should reject if f throws', () => {
+        let expected = {};
+        return delay(1).then(() => { throw expected; })
+            .then(assert.ifError, x => assert.strictEqual(x, expected));
+    });
+
+    it('should reject if r throws', () => {
+        let expected = {};
+        return delay(1).then(reject).then(null, () => { throw expected; })
+            .then(assert.ifError, x => assert.strictEqual(x, expected));
+    });
+
+});
