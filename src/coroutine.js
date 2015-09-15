f2a218d2650be30b795ef2cd2d8079a022f01539
Brian Cavalier <brian@hovercraftstudios.com>
2015-09-14 22:00:36 -0400

    Reduce coroutine object allocation

--- src/coroutine.js
+++ src/coroutine.js
@@ -1,41 +1,43 @@
 'use strict';
 
 export default function (refFor, iterator, promise) {
-    coStep(refFor, iterator.next, void 0, iterator, promise);
+    new Coroutine(refFor, iterator, promise).run();
     return promise;
 }
 
-function coStep(refFor, continuation, x, iterator, promise) {
-    try {
-        handle(refFor, continuation.call(iterator, x), iterator, promise);
-    } catch (e) {
-        promise._reject(e);
-    }
-}
-
-function handle(refFor, result, iterator, promise) {
-    if (result.done) {
-        return promise._resolve(result.value);
-    }
-
-    refFor(result.value)._runAction(new Next(refFor, iterator, promise));
-}
-
-class Next {
-    constructor(refFor, iterator, promise) {
-        this.refFor = refFor;
+class Coroutine {
+    constructor(resolve, iterator, promise) {
+        this.resolve = resolve;
         this.iterator = iterator;
         this.promise = promise;
     }
 
+    run() {
+        this.step(this.iterator.next, void 0);
+    }
+
+    step(continuation, x) {
+        try {
+            this.handle(continuation.call(this.iterator, x));
+        } catch (e) {
+            this.promise._reject(e);
+        }
+    }
+
+    handle(result) {
+        if (result.done) {
+            return this.promise._resolve(result.value);
+        }
+
+        this.resolve(result.value)._runAction(this);
+    }
+
     fulfilled(ref) {
-        let iterator = this.iterator;
-        coStep(this.refFor, iterator.next, ref.value, iterator, this.promise);
+        this.step(this.iterator.next, ref.value);
     }
 
     rejected(ref) {
-        let iterator = this.iterator;
-        coStep(this.refFor, iterator.throw, ref.value, iterator, this.promise);
+        this.step(this.iterator.throw, ref.value);
         return true;
     }
 }
