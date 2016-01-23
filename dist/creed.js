4956444223ba5ff8b562f217e5146a20f0f142e3
Brian Cavalier <brian@hovercraftstudios.com>
2016-01-22 22:26:00 -0500

    Update some dev deps

--- dist/creed.js
+++ dist/creed.js
@@ -158,49 +158,6 @@
         return TaskQueue;
     })();
 
-    var UNHANDLED_REJECTION$1 = 'unhandledRejection';
-
-    function makeEmitError() {
-        /*global process, self, CustomEvent*/
-        // istanbul ignore else */
-        if (isNode && typeof process.emit === 'function') {
-            // Returning falsy here means to call the default reportRejection API.
-            // This is safe even in browserify since process.emit always returns
-            // falsy in browserify:
-            return function (type, error) {
-                return type === UNHANDLED_REJECTION$1 ? process.emit(type, error.value, error) : process.emit(type, error);
-            };
-        } else if (typeof self !== 'undefined' && typeof CustomEvent === 'function') {
-            return (function (noop, self, CustomEvent) {
-                var hasCustomEvent;
-                try {
-                    hasCustomEvent = new CustomEvent(UNHANDLED_REJECTION$1) instanceof CustomEvent;
-                } catch (e) {
-                    hasCustomEvent = false;
-                }
-
-                return !hasCustomEvent ? noop : function (type, error) {
-                    var ev = new CustomEvent(type, {
-                        detail: {
-                            reason: error.value,
-                            promise: error
-                        },
-                        bubbles: false,
-                        cancelable: true
-                    });
-
-                    return !self.dispatchEvent(ev);
-                };
-            })(noop, self, CustomEvent);
-        }
-
-        // istanbul ignore next */
-        return noop;
-    }
-
-    // istanbul ignore next */
-    function noop() {}
-
     var UNHANDLED_REJECTION = 'unhandledRejection';
     var HANDLED_REJECTION = 'rejectionHandled';
 
@@ -248,6 +205,49 @@
             }
         }
     }
+
+    var UNHANDLED_REJECTION$1 = 'unhandledRejection';
+
+    function makeEmitError() {
+        /*global process, self, CustomEvent*/
+        // istanbul ignore else */
+        if (isNode && typeof process.emit === 'function') {
+            // Returning falsy here means to call the default reportRejection API.
+            // This is safe even in browserify since process.emit always returns
+            // falsy in browserify:
+            return function (type, error) {
+                return type === UNHANDLED_REJECTION$1 ? process.emit(type, error.value, error) : process.emit(type, error);
+            };
+        } else if (typeof self !== 'undefined' && typeof CustomEvent === 'function') {
+            return (function (noop, self, CustomEvent) {
+                var hasCustomEvent;
+                try {
+                    hasCustomEvent = new CustomEvent(UNHANDLED_REJECTION$1) instanceof CustomEvent;
+                } catch (e) {
+                    hasCustomEvent = false;
+                }
+
+                return !hasCustomEvent ? noop : function (type, error) {
+                    var ev = new CustomEvent(type, {
+                        detail: {
+                            reason: error.value,
+                            promise: error
+                        },
+                        bubbles: false,
+                        cancelable: true
+                    });
+
+                    return !self.dispatchEvent(ev);
+                };
+            })(noop, self, CustomEvent);
+        }
+
+        // istanbul ignore next */
+        return noop;
+    }
+
+    // istanbul ignore next */
+    function noop() {}
 
     // maybeThenable :: * -> boolean
     function maybeThenable(x) {
