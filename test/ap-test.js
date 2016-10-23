4f88093f47f9d3544b7c899b8c474148851a51bd
Brian Cavalier <brian@hovercraftstudios.com>
2016-10-23 16:27:42 -0400

    Update ap() to Fantasy Land 1.0
    BREAKING CHANGE: This changes the "argument" order for ap(), in
    accordance with the same breaking change to the FL spec 1.0.

--- test/ap-test.js
+++ test/ap-test.js
@@ -1,35 +1,35 @@
 import { describe, it } from 'mocha'
-import { fulfill } from '../src/main'
+import { delay } from '../src/main'
 import { assertSame } from './lib/test-util'
+
+const fulfill = x => delay(1, x)
 
 describe('ap', () => {
 	it('should satisfy identity', () => {
-		const v = fulfill({})
-		return assertSame(fulfill(x => x).ap(v), v)
+		const a = fulfill({})
+		return assertSame(a.ap(fulfill(x => x)), a)
 	})
 
 	it('should satisfy composition', () => {
 		const u = fulfill(x => 'u' + x)
 		const v = fulfill(x => 'v' + x)
-		const w = fulfill('w')
+		const a = fulfill('a')
 
 		return assertSame(
-			fulfill(f => g => x => f(g(x))).ap(u).ap(v).ap(w),
-			u.ap(v.ap(w))
+			a.ap(u.ap(v.map(f => g => x => f(g(x))))),
+			a.ap(u).ap(v)
 		)
 	})
 
 	it('should satisfy homomorphism', () => {
 		const f = x => x + 'f'
-		const x = 'x'
-		return assertSame(fulfill(f).ap(fulfill(x)), fulfill(f(x)))
+		const a = 'a'
+		return assertSame(fulfill(a).ap(fulfill(f)), fulfill(f(a)))
 	})
 
 	it('should satisfy interchange', () => {
-		const f = x => x + 'f'
-		const u = fulfill(f)
-		const y = 'y'
-
-		return assertSame(u.ap(fulfill(y)), fulfill(f => f(y)).ap(u))
+		const a = 'w'
+		const pf = fulfill(x => 'f' + x)
+		return assertSame(fulfill(a).ap(pf), pf.ap(fulfill(f => f(a))))
 	})
 })
