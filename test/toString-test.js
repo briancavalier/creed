8edc162f1b173b01fe11e60f1a54990e0c41d013
Brian Cavalier <brian@briancavalier.com>
2017-12-06 16:35:58 -0500

    test(toString): add toString resolution tests (#162)

--- test/toString-test.js
+++ test/toString-test.js
@@ -1,28 +1,43 @@
+import { Future, fulfill, future, never, reject } from '../src/Promise'
 import { describe, it } from 'mocha'
-import { fulfill, reject, Future, never } from '../src/Promise'
-import { getValue, getReason } from '../src/inspect'
+import { getReason, getValue } from '../src/inspect'
+
 import { eq } from '@briancavalier/assert'
+
+const verifyToStringResolved = (resolution) => {
+  const { resolve, promise } = future()
+  resolve(resolution)
+  eq(resolution.toString(), promise.toString())
+}
 
 describe('toString', () => {
   it('should indicate fulfilled promise', () => {
-    let p = fulfill('a')
+    const p = fulfill('a')
     eq(`[object Promise { fulfilled: ${getValue(p)} }]`,
             p.toString())
   })
 
   it('should indicate rejected promise', () => {
-    let p = reject(new Error('a'))
+    const p = reject(new Error('a'))
     eq(`[object Promise { rejected: ${getReason(p)} }]`,
             p.toString())
   })
 
   it('should indicate pending promise', () => {
-    let p = new Future()
+    const p = new Future()
     eq('[object Promise { pending }]', p.toString())
   })
 
+  it('should indicate resolution for promise resolved to fulfilled', () => {
+    verifyToStringResolved(fulfill('a'))
+  })
+
+  it('should indicate resolution for promise resolved to fulfilled', () => {
+    verifyToStringResolved(reject(new Error('a')))
+  })
+
   it('should indicate never', () => {
-    let p = never()
+    const p = never()
     eq('[object Promise { never }]', p.toString())
   })
 })
